e36e77ec4f6ca44441534bd04131796d9e41b1fe
ELDani45 <chilito45giron@gmail.com>
2026-06-15 22:29:04 -0500

    new commit

--- UI/views.py
+++ UI/views.py
@@ -1,41 +1,60 @@
+""" vista principal de la app """
+import os
+import time
 from asgiref.sync import async_to_sync
 from django.views.generic import FormView
+from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
-from django.contrib import messages  
-from .forms import FormUploadHwork
+from django.contrib import messages
 from scripts.main import main
-     
+from .forms import FormUploadHwork
 
 class HomePageView(FormView):
     """ Formulario para subir tarea """
     template_name = 'index.html'
     form_class = FormUploadHwork
 
-    success_url = reverse_lazy('home')  
+    success_url = reverse_lazy('home')
 
     def post(self, request, *args, **kwargs):
-        form = self.get_form_class()(files = request.FILES)
+        form = self.get_form_class()(files=request.FILES)
         print(form)
         try:
             if form.is_valid():
                 return self.form_valid(form)
-        except Exception as e:
+        except FileExistsError as e:
             messages.error(request, "No se ha enviado ningún archivo en el campo correspondiente.")
             print(e)
 
-            
         return self.form_invalid(form)
-            
 
     def form_valid(self, form):
-        # investgar sobre cleaned_data
-        archivo = form.cleaned_data.get('file_homework') 
+        archivo = form.cleaned_data.get('file_homework')
         nombre_archivo = archivo.name
+
+        ruta_tareas_windows = r"C:\Users\chili\OneDrive\Desktop\sena_docs\TAREAS"
+        fs = FileSystemStorage(location=ruta_tareas_windows)
+
+        if fs.exists(nombre_archivo):
+            try:
+                print(f"El archivo {nombre_archivo} eliminado")
+                fs.delete(nombre_archivo)
+            except PermissionError:
+                base, ext = os.path.splitext(nombre_archivo)
+                nombre_archivo = f"{base}_{int(time.time())}{ext}"
+                print(f"name_alter: {nombre_archivo}")
+        nombre_guardado = fs.save(nombre_archivo, archivo)
+        root_complet = fs.path(nombre_guardado)
         try:
-            async_to_sync(main)(nombre_archivo)
+            async_to_sync(main)(nombre_archivo, root_complet)
             messages.success(self.request, 'Tarea subida correctamente')
-        except Exception as e:
-            print("es de form valid?")
+            try:
+                if os.path.exists(root_complet):
+                    os.remove(root_complet)
+            except FileNotFoundError as e:
+                print(e)
+        except FileExistsError as e:
+            print("Error de Playwright:")
             print(e)
-        
+            messages.error(self.request, 'Ocurrió un error al procesar el script.')
         return super().form_valid(form)
--- up_hworks_sena/settings.py
+++ up_hworks_sena/settings.py
@@ -116,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'
+
+# Ruta física en tu computadora donde se guardan los archivos
+MEDIA_ROOT = BASE_DIR / 'media'
